250aae1edbf35c7ac510a8be3142b349398c7959
Jordan Hall <jordan@hall05.co.uk>
2026-07-18 11:57:30 +0100

    Use a genuinely zero-similarity regression query

--- tests/OmegaSearchTest.php
+++ tests/OmegaSearchTest.php
@@ -35,6 +35,6 @@ class OmegaSearchTest extends TestCase
     public function testBlankSearchReturnsNoResults()
     {
         $this->assertCount(0, $this->search()->query('   '));
-        $this->assertCount(0, $this->search()->query('no-match-at-all'));
+        $this->assertCount(0, $this->search()->query('qqqqqq'));
     }
 }
