69e31b9cb0c71e920133361aceacfad2ee22987f
Jordan Hall <jordan@hall05.co.uk>
2026-07-18 12:01:57 +0100

    Correct the zero-similarity regression query

--- tests/OmegaSearchTest.php
+++ tests/OmegaSearchTest.php
@@ -35,6 +35,6 @@ class OmegaSearchTest extends TestCase
     public function testBlankSearchReturnsNoResults()
     {
         $this->assertCount(0, $this->search()->query('   '));
-        $this->assertCount(0, $this->search()->query('qqqqqq'));
+        $this->assertCount(0, $this->search()->query('zzzzzz'));
     }
 }
